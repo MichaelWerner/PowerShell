077c06dfda54075d0a1ec22bbe30cd6879ce1d46
MichaelWerner <michael@4werners.net>
2021-03-25 09:33:00 -0400

    Format string with leading 0

--- GetBootInfo.ps1
+++ GetBootInfo.ps1
@@ -1,5 +1,5 @@
 $boottime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
 $up = (Get-Date) - $boottime
-$runtime = $up.Hours,$up.Minutes,$up.Seconds -join ":"
+$runtime = "{0:d2}:{1:d2}:{2:d2}" -f $up.Hours,$up.Minutes,$up.Seconds
 $info = "Booted at: {0}`nUptime: {1}" -f $boottime,$runtime
 $info
